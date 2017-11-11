f11445837367136f57666e7961f067f17764c749
Benji <thebkrcanbefound@gmail.com>
2017-11-11 15:36:50 -0800

    switch from other git repo due to git commit error

---
+++ client/client.js
@@ -0,0 +1,64 @@
+var notepad = angular.module('notepad', []);  //'ngRoute'
+
+
+//first time page opens should GET all notes
+  //Change to be a button to refresh later.
+// function mainController($scope, $http) {
+notepad.controller('mainController', ['$scope', '$http', function($scope, $http) {
+
+  //post request to server.
+    //pass the inputted text (as json?)
+  $scope.textToPost = {};
+
+  $scope.addNote = function() {
+    $http.post('/note', $scope.textToPost).then(function(response, error){
+    if (error) {
+      return console.log('error POST line 19 client', error)
+    }
+    console.log('succes POST??? line 21 client', response);
+  }, function(response, error){
+    if (error) {
+      console.log('error POST line 24 client', error)
+    } else if (response) {
+      console.log('SUCCESS POST line 26 client', $scope.textToPost); //****THIS DATA IS FROM INPUT FIELD :D******
+    }
+  })};
+
+
+//REFRESH BUTTON GET.
+  $scope.products = [];
+  $scope.retrieve = function() {
+    $http.get('/note').then(function(response, error){ //error func
+      if (error) {
+        console.log('error GET line 34 client', error)
+      } else if(response) {
+        $scope.products = response.data;
+        $scope.products.reverse();
+        console.log('SUCCESS GET line 37 client');//response is data sent back from server(aka all db entries)
+      }
+    }, function(response, error){ //success func
+      if (error) {
+        console.log('error GET line 41 client', error)
+      } else if (response) {
+        console.log('SUCCESS GET line 43 client', response);
+      }
+    });
+  }
+
+
+//TODO after page is launched
+  // CRYPTODATA.  Planned to have it in server but http undefined. look into later
+  // $scope.crypto = function() {
+  //   $http.get('https://api.coinmarketcap.com/v1/ticker/').then(function(response, error){
+  //     if (error) {
+  //       console.log('error GET line 52 client', error)
+  //     } else if (response) {
+  //       console.log('SUCCESS GET line 54 client', response);
+  //     }
+  //     response.send('/crypto.html')
+  //   });
+  // }
+
+
+
+}]);
---
+++ server.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+var http = require('http');
+
+// var router = express.Router();
+
+
+var mongoose = require('mongoose');
+var NoteSchema = require('./db').NoteSchema;
+//**TODO: create own database**
+
+app.use(express.static(__dirname + '/client'));
+// app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+// app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+// app.use(methodOverride());
+
+
+//GET
+app.get('/note', function(req, res) {   //note
+  NoteSchema.find(function(error, data) {
+    if (error) {
+      console.log('ERROR line 26 GET = ', error);
+    } else {
+      console.log('Success: GET from DB data') //, data[0].text
+      // res.send(data); //should it be .send?
+      res.send(data);
+    }
+  });
+});
+
+
+
+//POST
+app.post('/note', function(req, res) {
+  //get text from input field of client side: answer: body.text
+  //send data to mongo as Note schema
+  //check if it has been sent
+  addNoteDB(req.body.text);
+  // console.log('hitting line 52 POST server', req.body.text)
+  res.status(200).end();
+})
+
+var addNoteDB = function(noteText) {
+  var newNote = new NoteSchema({ text: noteText });
+
+  newNote.save(function(error, noteText) {
+    if (error) {
+      return console.log('ERROR line 50 server =', error)
+    }
+    // console.log('new note has been added to DB = ', noteText)
+  });
+}
+
+
+
+//HIGHLY Speculative feature: get Cryptocurrency pricing data
+// app.get('/cryptoData', function(req, res) {
+//   app.get('https://api.coinmarketcap.com/v1/ticker/', function(req, res) {
+//     if (req) {
+//       console.log('error GET line 67 server', req)
+//     } else if(res) {
+//       console.log('SUCCESS GET line 69 server', res);//response is data sent back from server(aka all db entries)
+//     }
+//   });
+// });
+
+
+
+
+var port = 8080;
+app.listen(port);
+console.log("App listening on port " + port);
